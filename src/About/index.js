a987671b0ac5045de914564e145a868f393e8e6a
Scott T <scottt@ool-ad020371.dyn.optonline.net>
2018-09-25 18:06:25 -0400

    added profile picture

--- src/About/index.js
+++ src/About/index.js
@@ -5,7 +5,12 @@ export default class About extends Component {
   render() {
     return (
       <div>
+        <div className="headshot-container">
+          <img className="headshot-image" src="https://i.imgur.com/OXY5vCA.png"/>
+        </div>
+
         <h2 className="about-header"> Scott Tsai</h2>
+
         <div className="bio-container">
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
